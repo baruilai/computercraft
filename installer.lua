f7aeb0c4293e2bc15d54130b04eab6e8a0cdd144
baruilai <baruilai@gmail.com>
2018-09-02 13:04:49 +0200

    updated installer, going to test

--- installer.lua
+++ installer.lua
@@ -3,6 +3,10 @@
 local program_list = {
 	require.URL = "https://raw.githubusercontent.com/baruilai/Computercraft/master/api/require.lua";
 	require.name = "require";
+	test.URL = "https://raw.githubusercontent.com/baruilai/Computercraft/master/test.lua";
+	test.name = "test";
+	move.URL = "https://raw.githubusercontent.com/baruilai/Computercraft/master/api/move";
+	move.name = "move";
 }
 
 require_URL = "https://raw.githubusercontent.com/baruilai/Computercraft/master/api/require.lua"
@@ -18,4 +22,5 @@ local function gitGet(git_URL, file_name)
 	end
 end
 
-gitGet(require_URL, require.name)
+gitGet(move.URL, move.name)
+gitGet(test.URL, test.name)
